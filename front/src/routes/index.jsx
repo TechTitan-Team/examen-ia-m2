d7077bd5887bf52850941054c808fdf369882bd6
Ando <ramamonjizafymanitra06@gmail.com>
2025-12-18 15:29:23 +0300

    add router

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/Common/Layout/Layout";
 import Home from "@/Pages/Home/Home";
+import Landing from "../Pages/LandingPage/landing-page";
 // import Landing from "../Pages/components/landing-page";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <Layout />,
     children: [
       {
@@ -14,5 +15,9 @@ export const router = createBrowserRouter([
         // element: <Landing/>
       },
     ],
+  },
+  {
+    path: "/",
+    element: <Landing />
   },
 ]);
